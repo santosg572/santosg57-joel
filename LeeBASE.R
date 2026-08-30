e5ad4d6eef049025751f54bf9c8d737c05e3e3a8
santosg572 <santosg572@gmail.com>
2026-08-30 10:48:42 -0600

    Test comité

--- LeeBASE.R
+++ LeeBASE.R
@@ -6,21 +6,40 @@ tabla <- read.csv('BASE_2708_V1.csv')
 
 attach(tabla)
 
-datosN <- cbind(Nombre, X, Age, Group, SCL90_1)
+SCL90_1[54] = ''
 
-#print(datosN)
+datosN <- cbind(No.[1:55], Nombre[1:55], X[1:55], Age[1:55], Group[1:55], SCL90_1[1:55])
 
+k = rep(0,10)
+Res = c()
 for (i in 1:55){
   print(paste('================= ', i, ' ================='))
   dd = SCL90_1[i]
   dd = as.numeric(dd)
   print(dd)
   if (!(is.na(dd))){
-    vec = as.numeric(tabla[i, 7:96])
-    res = calculoSCL90(vec)
-    print(dd)
-    print(vec)
-    print(res)
+    if (i != 54){
+      vec = as.numeric(tabla[i, 7:96])
+      res = round(calculoSCL90(vec),3)
+      print(dd)
+      print(vec)
+      print(res)
+      Res = rbind(Res, res)
+    } else {
+        Res = rbind(Res, k)
+    }  
+  } else{
+    Res = rbind(Res, k)
   }
 }
+
+Res = as.matrix(Res)
+
+print(dim(Res))
+
+datosN <- cbind(datosN, Res)
+
+rownames(datosN) <- NULL
+
+print(datosN)
 
