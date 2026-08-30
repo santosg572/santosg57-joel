2426841292f6c0db5f7cab129f1ca00d4ff72f99
santosg572 <santosg572@gmail.com>
2026-08-30 10:07:18 -0600

    Test comité

--- LeeBASE.R
+++ LeeBASE.R
@@ -4,28 +4,23 @@ source('Fun_calculaSCL90.R')
 
 tabla <- read.csv('BASE_2708_V1.csv')
 
-titulos <- names(tabla)
+attach(tabla)
 
-print(titulos[7:96])
+datosN <- cbind(Nombre, X, Age, Group, SCL90_1)
 
-nn = dim(tabla)
+#print(datosN)
 
-print(nn)
-
-suj = as.numeric(tabla[2,7:96])
-
-for (i in 1:nn[1]){
-  ss = tabla[i,]
-  nombre = as.character(ss[1])
-  scl90 = as.numeric(ss[7:96])
-  if (!is.na(scl90[1])){
-    print(nombre)
-    print(scl90)
-    res = calculoSCL90(scl90)
+for (i in 1:55){
+  print(paste('================= ', i, ' ================='))
+  dd = SCL90_1[i]
+  dd = as.numeric(dd)
+  print(dd)
+  if (!(is.na(dd))){
+    vec = as.numeric(tabla[i, 7:96])
+    res = calculoSCL90(vec)
+    print(dd)
+    print(vec)
     print(res)
   }
 }
-
-
-
 
