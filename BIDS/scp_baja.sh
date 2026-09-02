de900f7800b18b6e2addce1a9c1b0e1d1e4b3098
santosg572 <santosg572@gmail.com>
2026-09-02 09:50:56 -0600

    Test comité

---
+++ BIDS/scp_baja.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scp -r santosg@172.24.220.150:/misc/fmenaj/santosg/Joel_Martinez/BIDS/dataset* .
+scp -r santosg@172.24.220.150:/misc/fmenaj/santosg/Joel_Martinez/BIDS/participan* .
+scp -r santosg@172.24.220.150:/misc/fmenaj/santosg/Joel_Martinez/BIDS/task-* .
+
+
